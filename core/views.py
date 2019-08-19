cf3f9a117a910a07d8062e85325f7d3f740fd8c5
Michael <phamm89@gmail.com>
2019-08-18 23:57:42 -0400

    Creating Project

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Views Created Here
+def index(request):
+    """View function for home page of site."""
+    
+    
+    return render(request, 'index.html')
